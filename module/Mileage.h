2ed656a882e8519417f4e1f6bd5810bbd57af910
mutotaka0426 <hm17058@student.miyazaki-u.ac.jp>
2021-06-03 20:56:18 +0900

    Mileageクラスのビルドが通るように修正

--- module/Mileage.h
+++ module/Mileage.h
@@ -10,9 +10,6 @@
 
 class Mileage {
  public:
-  // インスタンスを生成する関数
-  static Mileage& getInstance();
-
   /**
    ** @fn　double calculateMileage(int rightAngle, int leftAngle);
    ** @brief 走行体全体の累計走行距離を計算する
@@ -20,25 +17,18 @@ class Mileage {
    ** @param leftAngle 左タイヤの回転角度[deg]
    ** @return 走行体全体の累計走行距離[mm]
    **/
-  double calculateMileage(int rightAngle, int leftAngle);
+  static double calculateMileage(int rightAngle, int leftAngle);
 
  private:
-  // コンストラクタ
-  Mileage();
-  // コピーコンストラクタ
-  Mileage(const Mileage& other);
-  // 代入演算子
-  Mileage& operator=(const Mileage& other);
-
   /**
    ** @fn double calculateWheelMileage(int angle);
    ** @brief タイヤの累計走行距離を計算する
    ** @param angle タイヤの回転角度[deg]
    ** @return タイヤの累計走行距離[mm]
    **/
-  double calculateWheelMileage(int angle);
+  static double calculateWheelMileage(int angle);
 
-  const double radius;  //タイヤの半径[mm]
-
+  // static constexpr double radius = 45;  //タイヤの半径[mm]
+  static const double RADIUS;
 };
 #endif
