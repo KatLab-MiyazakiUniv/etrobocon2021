dbd8a2e284542283cfdab3fa48493f97894624a1
Takahiro55555 <tkhr.corei7@gmail.com>
2021-06-25 00:21:41 +0900

    add: sim-test とりあえず、動作を確認

---
+++ scripts/sim-test-wsl.sh
@@ -0,0 +1,130 @@
+#!/bin/bash -eu
+
+# @file: sim-test-wsl.sh
+# @author: Takahiro55555
+# @brief: 設定ファイルを適用した後、シミュレータを動作させる
+#         WSL で動作させることしか考えていない
+
+### 以下、設定  ###
+APP_NAME="etrobocon2021"
+SETTINGS_FILES_DIR="`pwd`/sim-settings"
+DEFAULT_SETTINGS_FILE_NAME="${SETTINGS_FILES_DIR}/default.json"  # デフォルトのシミュレータ設定ファイル名
+### 以上、設定  ###
+
+# WSL であるかどうかを確認する
+if [ ! -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
+    echo "このスクリプトは、WSL上でしか動作しません"
+    exit 1
+fi
+
+# 設定ファイルのフォーマットを確認する
+if [[ ! "$1" =~ ^.*/[lr]/[0-9a-fA-F_\-]+\.json$ ]]; then
+    echo "Format Error (File path): ${1}"
+    echo 'Expected File Path Regex: ^.*/[lr]/[0-9a-fA-F_\-]+\.json$'
+    exit 1
+fi
+
+# 上書きするための設定ファイルへのパスをコマンドライン引数から取得する
+OVERWRITE_SETTINGS_FILE_NAME="`pwd`/$1"
+if [[ "$1" =~ ^/.+$ ]]; then
+    OVERWRITE_SETTINGS_FILE_NAME="$1"
+elif [[ "$1" =~ ^\./.+$ ]]; then
+    OVERWRITE_SETTINGS_FILE_NAME="`pwd``echo $1 | cut -b 2-`"
+fi
+
+# ファイルが見つからない場合、プログラムを終了する
+if [ ! -f "${OVERWRITE_SETTINGS_FILE_NAME}" ]; then
+    echo "File Not Found: ${OVERWRITE_SETTINGS_FILE_NAME}"
+    exit 1
+fi
+
+# etrobo独自の環境変数の確認
+if [[ -z "$ETROBO_ROOT" ]]; then
+    ETROBO_ROOT=`echo 'echo $ETROBO_ROOT' | ${HOME}/startetrobo shell | tail -n 1`
+fi
+if [[ -z "$ETROBO_HRP3_WORKSPACE" ]]; then
+    $ETROBO_HRP3_WORKSPACE=`echo 'echo $ETROBO_HRP3_WORKSPACE' | ${HOME}/startetrobo shell | tail -n 1`
+fi
+
+# 設定ファイルのあるディレクトリ名から左右どちらのコースで走行するか決定する
+COURSE=`echo $1 | sed -r 's/.*\/([rl])\/.+/\1/g'`
+
+# 設定ファイル名から番号を取得する
+NAME=`echo $1 | sed -r 's/.*\/[rl]\/([0-9a-fA-F_\-]+)\.json/\1/g'`
+
+# ファイルが見つからない場合、プログラムを終了する
+if [ ! -f "${DEFAULT_SETTINGS_FILE_NAME}" ]; then
+    echo "File Not Found: ${DEFAULT_SETTINGS_FILE_NAME}"
+    exit 1
+fi
+
+# 設定ファイルから、キャプチャディレクトリへのパスを取得する
+BASE_CAPTURE_DIR_WIN=$(jq -sc add ${DEFAULT_SETTINGS_FILE_NAME} ${OVERWRITE_SETTINGS_FILE_NAME} | jq -r '.captureDir')
+if [[ "$BASE_CAPTURE_DIR_WIN" =~ ^$ ]]; then
+    UBUNTU_VER=$(lsb_release -a | grep -oE '^Release:\s*[0-9]{2}\.[0-9]{2}' | cut -f 2)
+    BASE_CAPTURE_DIR_WIN="\\\\wsl\$\\Ubuntu-${UBUNTU_VER}\\tmp\\etrobo\\capture"
+fi
+
+# キャプチャディレクトリ配下に生成するディレクトリ名
+if [[ "$2" =~ ^$ ]]; then
+    CAPTURE_DIR_WIN="${BASE_CAPTURE_DIR_WIN}\\`date '+%Y-%m-%d'`\\`date '+%H-%M-%S'`\\${COURSE}\\${NAME}"
+else
+    if [[ "$2" =~ ^([0-9a-zA-Z_\-]+)(\\[0-9a-zA-Z_\-]+)?$ ]]; then
+        CAPTURE_DIR_WIN="${BASE_CAPTURE_DIR_WIN}\\$2\\${COURSE}\\${NAME}"
+    else
+        echo "Format Error (File path): ${2}"
+        echo 'Expected File Path Regex: ^([0-9a-zA-Z_\-]+)(\\[0-9a-zA-Z_\-]+)?$'
+        exit 1
+    fi
+fi
+
+if [[ "${CAPTURE_DIR_WIN}" =~ ^\\\\wsl.+$ ]]; then
+    CAPTURE_DIR_WSL=$(echo "${CAPTURE_DIR_WIN}" | sed -r 's/^\\{2}wsl\$\\Ubuntu-[0-9\.]+\\/\\/g' | sed -e 's/\\/\//g')
+elif [[ ${CAPTURE_DIR_WIN} =~ ^[A-Z]:(\\.*)+$ ]]; then
+    CAPTURE_DIR_WSL="/mnt/`echo ${CAPTURE_DIR_WIN,} | cut -b 1`"
+    CAPTURE_DIR_WSL+=$(echo "${CAPTURE_DIR_WIN}" | sed -r 's/[A-Z]:(.+)/\1/g' | sed -e 's/\\/\//g')
+fi
+
+# 設定ファイルの書き込み
+mkdir -p ${ETROBO_HRP3_WORKSPACE}/simdist/${APP_NAME};
+EDITED_SETTINGS_FILE_NAME="${ETROBO_HRP3_WORKSPACE}/simdist/${APP_NAME}/settings.json"
+CAPTURE_DIR_WIN_JSON_ESCAPED=$(echo "$CAPTURE_DIR_WIN" | sed -e 's/\\/\\\\/g')
+jq -sc add ${DEFAULT_SETTINGS_FILE_NAME} ${OVERWRITE_SETTINGS_FILE_NAME} | \
+    jq ".captureDir=\"${CAPTURE_DIR_WIN_JSON_ESCAPED}\"" > "${EDITED_SETTINGS_FILE_NAME}"
+CAPTURE_RATE=$(jq -r '.captureRate' ${EDITED_SETTINGS_FILE_NAME})
+
+if [[ ${CAPTURE_RATE} -ne 0 ]]; then
+    mkdir -p $CAPTURE_DIR_WSL
+fi
+EXEC_COMMANDS="echo '[ script: settings file name: ${1} ]'; "
+EXEC_COMMANDS+="echo '[ script: captureRate: ${CAPTURE_RATE} ]'; "
+EXEC_COMMANDS+="curl -X POST -H \"Content-Type: application/json\" -d @${EDITED_SETTINGS_FILE_NAME} http://localhost:54000; "
+EXEC_COMMANDS+="make ${COURSE} noset app=${APP_NAME} sim up; "
+echo ${EXEC_COMMANDS} | ${HOME}/startetrobo shell
+
+### シミュレータ映像のキャプチャが有効化されていた場合 ###
+if [[ ${CAPTURE_RATE} -eq 1 ]]; then
+    FRAME_RATE=60
+elif [[ ${CAPTURE_RATE} -eq 2 ]]; then
+    FRAME_RATE=30
+elif [[ ${CAPTURE_RATE} -eq 3 ]]; then
+    FRAME_RATE=20
+elif [[ ${CAPTURE_RATE} -eq 4 ]]; then
+    FRAME_RATE=15
+else
+    # シミュレータ映像のキャプチャが有効化されていなかった場合、スクリプトを終了する
+    exit 0
+fi
+# キャプチャ映像を MP4 に変換する
+cd $CAPTURE_DIR_WSL
+cd ../../
+ffmpeg -r ${FRAME_RATE} \
+       -i ${CAPTURE_DIR_WSL}/L_%08d.png \
+       -vcodec libx264 \
+       -pix_fmt yuv420p \
+       -r ${FRAME_RATE} \
+       ${COURSE}-${NAME}.mp4
+
+if [[ "$3" == 'true' ]]; then
+    (cd $CAPTURE_DIR_WSL; cd ../../ ; if [ -f .isOpened ]; then exit 0; fi; touch .isOpened; explorer.exe .)
+fi
---
+++ sim-test-all.sh
@@ -0,0 +1,169 @@
+#!/bin/bash -eu
+
+### 以下、設定 ###
+LOG_FILES_DIR="`pwd`/logs/`date '+%Y-%m-%d'`/`date '+%H-%M-%S'`"  # ログを格納するディレクトリ名
+MD_FILE_NAME='result.txt'  # マークダウン形式で出力するファイル
+CSV_FILE_NAME='result.csv'  # CSV 形式で出力するファイル
+### 以上、設定 ###
+
+# このシェルスクリプトを Ctrl-C で強制終了した際、
+# バックグランドで実行しているプロセスも強制終了する
+trap 'kill $(jobs -p)' EXIT
+
+### 以下、必要なパッケージの確認・インストール ###
+INSTALL_PACKAGE_LIST=""
+if [ `dpkg -l | grep '^ii' | grep ffmpeg | wc -l` -eq "0" ]; then
+    INSTALL_PACKAGE_LIST="ffmpeg "
+fi
+
+if [ `dpkg -l | grep '^ii' | grep -oE "\sjq\s" | wc -l` -eq "0" ]; then
+    INSTALL_PACKAGE_LIST+="jq "
+fi
+
+if [ "${INSTALL_PACKAGE_LIST}" != "" ]; then
+    echo "Install additional packages (${INSTALL_PACKAGE_LIST})"
+    sudo apt update && sudo apt install $INSTALL_PACKAGE_LIST
+fi
+### 以上、必要なパッケージの確認・インストール ###
+
+
+
+### 以下、シミュレータを実行し、結果を集計する ###
+mkdir -p ${LOG_FILES_DIR};
+CAPTURE_DIR=`date '+%Y-%m-%d'`\\`date '+%H-%M-%S'`
+echo 'sim' | ${HOME}/startetrobo shell
+ls sim-settings/?/* | \
+    xargs -L1 -I{} ./scripts/sim-test-wsl.sh {} $CAPTURE_DIR true | \
+    awk -v md_file_path=${LOG_FILES_DIR}/${MD_FILE_NAME} \
+        -v csv_file_name=${LOG_FILES_DIR}/${CSV_FILE_NAME} '
+    BEGIN {
+        counter = 0;
+        run_time_sum = 0;
+        goaled_num = 0;
+        settings_file_name = "Unknown";
+        capture_rate = "Unknown";
+    }
+    {
+        if($0 ~ /\[ script: settings file name: .+\]/){
+            settings_file_name = $6;
+            print "\n" $0;
+        }
+
+        if($0 ~ /\[ script: captureRate: [0-9] \]/){
+            if ($4 == 0) {
+                capture_rate = "OFF";
+            } else if ($4 == 1) {
+                capture_rate = "60fps";
+            } else if ($4 == 2) {
+                capture_rate = "30fps";
+            } else if ($4 == 3) {
+                capture_rate = "20fps";
+            } else if ($4 == 4) {
+                capture_rate = "15fps";
+            }
+            print $0;
+        }
+
+        if($0 ~ /\[ launcher: ready to start on ((right)|(left)) course \]/){
+            counter++;
+            STATISTICS[counter]["course"]="";
+            STATISTICS[counter]["settings_file_name"]=settings_file_name;
+            STATISTICS[counter]["start_gate"]="false";
+            STATISTICS[counter]["gate_1"]="false";
+            STATISTICS[counter]["gate_2"]="false";
+            STATISTICS[counter]["goal_time"]="none";
+            STATISTICS[counter]["capture_rate"]=capture_rate;
+            settings_file_name = "Unknown";
+            capture_rate = "Unknown";
+            print $0;
+        }
+
+        if($0 ~ /\[ launcher: ready to start on right course \]/){
+            STATISTICS[counter]["course"]="Right";
+        }else if($0 ~ /\[ launcher: ready to start on left course \]/){
+            STATISTICS[counter]["course"]="Left";
+        }
+
+        if($0 ~ /^\[ launcher: ((left)|(right)): passed Start Gate \]$/){
+            STATISTICS[counter]["start_gate"]="true";
+            print $0;
+        }
+
+        if($0 ~ /^\[ launcher: ((left)|(right)): passed Gate 1 \]$/){
+            STATISTICS[counter]["gate_1"]="true";
+            print $0;
+        }
+
+        if($0 ~ /^\[ launcher: ((left)|(right)): passed Gate 2 \]$/){
+            STATISTICS[counter]["gate_2"]="true";
+            print $0;
+        }
+
+        if($0 ~ /^\[ launcher: ((left)|(right)): GOAL!  Goal Time: [0-9]*\.[0-9]* \]$/){
+            STATISTICS[counter]["goal_time"]=$7;
+            run_time_sum += $7;
+            goaled_num++;
+            print $0;
+        }
+    }
+    END {
+        if(counter == 0){
+            print "ERR: シミュレータログの検出に失敗";
+            exit 1;
+        }
+
+
+        ### 以下、CSVファイル生成 ###
+        # ヘッダ行の作成
+        for (i in STATISTICS[1]) {
+            printf "%s,", i >> csv_file_name
+        }
+        printf "\n" >> csv_file_name
+
+        # データ行の作成
+        for (i in STATISTICS) {
+            for (j in STATISTICS[i]) {
+                printf "%s,", STATISTICS[i][j] >> csv_file_name
+            }
+            printf "\n" >> csv_file_name
+        }
+        ### 以上、CSVファイル生成 ###
+
+
+        ### 以下、Markdown形式ファイル生成(.txt ファイル) ###
+        ## 各実験結果テーブルの作成
+        # ヘッダー行の作成
+        print "### 各実験結果" > md_file_path;
+        print "| 番号 | コース | Start Gate | Gate 1 | Gate 2 | 走行時間 | キャプチャ | 設定ファイル名 |" >> md_file_path;
+        print "| ---- | ----- | ---------- | ------ | ------ | ------- | ---------- | ------------- |" >> md_file_path;
+
+        # データ行の作成
+        for (i in STATISTICS) {
+            printf "| %s ", i >> md_file_path;
+            printf "| %s ", STATISTICS[i]["course"] >> md_file_path;
+            printf "| %s ", STATISTICS[i]["start_gate"] >> md_file_path;
+            printf "| %s ", STATISTICS[i]["gate_1"] >> md_file_path;
+            printf "| %s ", STATISTICS[i]["gate_2"] >> md_file_path;
+            printf "| %s ", STATISTICS[i]["goal_time"] >> md_file_path;
+            printf "| %s ", STATISTICS[i]["capture_rate"] >> md_file_path;
+            printf "| %s |\n", STATISTICS[i]["settings_file_name"] >> md_file_path;
+        }
+        print "\n__Bool値は、各ゲートを通過したか否かを示している__\n" >> md_file_path;
+
+        ## 集計テーブルの作成
+        run_time_average = run_time_sum/goaled_num;
+        goal_persentage = goaled_num/counter*100;
+        print "### 集計結果" > md_file_path;
+        print "\n| 走行エリア平均 | 成功率 |" >> md_file_path;
+        print "| ------------- | ----- |" >> md_file_path;
+        printf "| %3.3f [s] (n=%d)    ", run_time_average, goaled_num >> md_file_path;
+        printf "| %3.3f [%] (n=%d) |\n", goal_persentage , counter    >> md_file_path;
+        ### 以上、Markdown形式ファイル生成(.txt ファイル) ###
+
+
+        ### 以下、標準出力 ###
+        print "走行エリア平均 : " run_time_average " [s] (n=" goaled_num ")";
+        print "成功率        : " goal_persentage " [%] (n=" counter ")";
+        ### 以上、標準出力 ###
+    }' && (cd ${LOG_FILES_DIR}; explorer.exe ${MD_FILE_NAME})
+### 以上、シミュレータを実行し、結果を集計する ###
