09e4703ddea15036fda0ea42c6dfe6d67be042df
Takahiro55555 <tkhr.corei7@gmail.com>
2021-06-25 00:42:10 +0900

    refactor: コメントを追記

--- sim-test-all.sh
+++ sim-test-all.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -eu
 
+# @file: sim-test-wsl.sh
+# @author: Takahiro55555
+# @brief: 設定ファイルを適用した後、シミュレータを動作させる
+#         WSL で動作させることしか考えていない
+#         sim-settings/l 又は、sim-settings/r ディレクトリ中の設定ファイルの数だけテストを行う
+
 ### 以下、設定 ###
 LOG_FILES_DIR="`pwd`/logs/`date '+%Y-%m-%d'`/`date '+%H-%M-%S'`"  # ログを格納するディレクトリ名
 MD_FILE_NAME='result.txt'  # マークダウン形式で出力するファイル
@@ -31,12 +37,19 @@ fi
 ### 以下、シミュレータを実行し、結果を集計する ###
 mkdir -p ${LOG_FILES_DIR};
 CAPTURE_DIR=`date '+%Y-%m-%d'`\\`date '+%H-%M-%S'`
+
+# シミュレータを起動
 echo 'sim' | ${HOME}/startetrobo shell
+
+# 設定ファイルをシミュレータに反映した後、プログラムを実行する
+# awk コマンドの -v オプションで awk 中のスクリプトにシェル変数を渡している
 ls sim-settings/?/* | \
     xargs -L1 -I{} ./scripts/sim-test-wsl.sh {} $CAPTURE_DIR true | \
     awk -v md_file_path=${LOG_FILES_DIR}/${MD_FILE_NAME} \
         -v csv_file_name=${LOG_FILES_DIR}/${CSV_FILE_NAME} '
     BEGIN {
+        # ループの最初に1度だけ呼び出されるブロック
+        # ここでは、変数の初期化を行っている
         counter = 0;
         run_time_sum = 0;
         goaled_num = 0;
@@ -44,11 +57,17 @@ ls sim-settings/?/* | \
         capture_rate = "Unknown";
     }
     {
+        # 標準出力から1行出力されるたびに呼び出されるブロック（ループ）
+        # $0 に標準出力の1行が設定されている
+        # $1, $2, $3, ... $n には標準出力の1行をスペースで区切ったものが順に設定されている
+
+        # 設定ファイル名を取得
         if($0 ~ /\[ script: settings file name: .+\]/){
             settings_file_name = $6;
             print "\n" $0;
         }
 
+        # シミュレータをキャプチャする際のフレームレートを取得
         if($0 ~ /\[ script: captureRate: [0-9] \]/){
             if ($4 == 0) {
                 capture_rate = "OFF";
@@ -64,6 +83,7 @@ ls sim-settings/?/* | \
             print $0;
         }
 
+        # 連想配列の初期化
         if($0 ~ /\[ launcher: ready to start on ((right)|(left)) course \]/){
             counter++;
             STATISTICS[counter]["course"]="";
@@ -78,27 +98,32 @@ ls sim-settings/?/* | \
             print $0;
         }
 
+        # コース情報を取得
         if($0 ~ /\[ launcher: ready to start on right course \]/){
             STATISTICS[counter]["course"]="Right";
         }else if($0 ~ /\[ launcher: ready to start on left course \]/){
             STATISTICS[counter]["course"]="Left";
         }
 
+        # スタートゲート通貨情報を取得
         if($0 ~ /^\[ launcher: ((left)|(right)): passed Start Gate \]$/){
             STATISTICS[counter]["start_gate"]="true";
             print $0;
         }
 
+        # ゲート1通過情報を取得
         if($0 ~ /^\[ launcher: ((left)|(right)): passed Gate 1 \]$/){
             STATISTICS[counter]["gate_1"]="true";
             print $0;
         }
 
+        # ゲート2通過情報を取得
         if($0 ~ /^\[ launcher: ((left)|(right)): passed Gate 2 \]$/){
             STATISTICS[counter]["gate_2"]="true";
             print $0;
         }
 
+        # ゴール通過情報を取得
         if($0 ~ /^\[ launcher: ((left)|(right)): GOAL!  Goal Time: [0-9]*\.[0-9]* \]$/){
             STATISTICS[counter]["goal_time"]=$7;
             run_time_sum += $7;
@@ -107,6 +132,8 @@ ls sim-settings/?/* | \
         }
     }
     END {
+        # ループの最後に1度だけ呼び出されるブロック
+        
         if(counter == 0){
             print "ERR: シミュレータログの検出に失敗";
             exit 1;
@@ -166,4 +193,5 @@ ls sim-settings/?/* | \
         print "成功率        : " goal_persentage " [%] (n=" counter ")";
         ### 以上、標準出力 ###
     }' && (cd ${LOG_FILES_DIR}; explorer.exe ${MD_FILE_NAME})
+    # 上記の && 以降で生成した Markdown 形式のファイルを開く処理を行っている
 ### 以上、シミュレータを実行し、結果を集計する ###
