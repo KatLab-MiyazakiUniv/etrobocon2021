f92b152a773b674525c3f79934c7aadcaee9590c
kodama0720 <bnuy1221@gmail.com>
2021-06-27 11:09:29 +0900

    コメント等の修正

--- module/Rotation.cpp
+++ module/Rotation.cpp
@@ -17,27 +17,32 @@ void Rotation::rotateLeft(int angle, int pwm)
   double currentLeftDistance = 0;
   double currentRightDistance = 0;
   double targetDistance = M_PI * TREAD * abs(angle) / 360;  //弧の長さ
+  //目標距離（呼び出し時の走行距離 ± 指定された回転量に必要な距離）
   double targetLeftDistance
       = Mileage::calculateWheelMileage(measurer.getLeftCount()) - targetDistance;
   double targetRightDistance
       = Mileage::calculateWheelMileage(measurer.getRightCount()) + targetDistance;
 
-  //両輪がtargetDistanceに到達するまでループ
+  //両輪が目標距離に到達するまでループ
   while(leftSign != 0 || rightSign != 0) {
+    //現在の走行距離を取得
     currentLeftDistance = Mileage::calculateWheelMileage(measurer.getLeftCount());
     currentRightDistance = Mileage::calculateWheelMileage(measurer.getRightCount());
+    //目標距離に到達したらpwm値にかける値を0にする
     if(currentLeftDistance <= targetLeftDistance) {
       leftSign = 0;
     }
     if(currentRightDistance >= targetRightDistance) {
       rightSign = 0;
     }
-
+    //モータのPWM値をセット
     controller.setLeftMotorPwm(abs(pwm) * leftSign);
     controller.setRightMotorPwm(abs(pwm) * rightSign);
 
+    // 10ミリ秒待機
     controller.sleep();
   }
+  //モータの停止
   controller.stopMotor();
 }
 
@@ -57,34 +62,39 @@ void Rotation::rotateRight(int angle, int pwm)
 
   //両輪が目標距離に到達するまでループ
   while(leftSign != 0 || rightSign != 0) {
+    //現在の走行距離を取得
     currentLeftDistance = Mileage::calculateWheelMileage(measurer.getLeftCount());
     currentRightDistance = Mileage::calculateWheelMileage(measurer.getRightCount());
-
+    //目標距離に到達したらpwm値にかける値を0にする
     if(currentLeftDistance >= targetLeftDistance) {
       leftSign = 0;
     }
     if(currentRightDistance <= targetRightDistance) {
       rightSign = 0;
     }
-
+    //モータのPWM値をセット
     controller.setLeftMotorPwm(abs(pwm) * leftSign);
     controller.setRightMotorPwm(abs(pwm) * rightSign);
 
+    // 10ミリ秒待機
     controller.sleep();
   }
+  //モータの停止
   controller.stopMotor();
 }
 
 //設定された角度とPWM値で右タイヤを軸に前方へピボットターンする
 void Rotation::turnForwardRightPivot(int angle, int pwm)
 {
-  double Distance = 2 * M_PI * TREAD * angle / 360;  //弧の長さ r:半径 angle:中心角
-  double targetDistance = Mileage::calculateWheelMileage(measurer.getLeftCount()) + Distance;
+  double distance = 2 * M_PI * TREAD * angle / 360;  //弧の長さ
+  double targetDistance = Mileage::calculateWheelMileage(measurer.getLeftCount()) + distance;
 
+  //目標距離を超えるまでループ
   while(true) {
-    double currentArcDistance = Mileage::calculateWheelMileage(measurer.getLeftCount());
+    //現在の距離を取得
+    double currentDistance = Mileage::calculateWheelMileage(measurer.getLeftCount());
 
-    if(currentArcDistance >= targetDistance) {
+    if(currentDistance >= targetDistance) {
       break;
     }
 
@@ -102,13 +112,14 @@ void Rotation::turnForwardRightPivot(int angle, int pwm)
 //設定された角度とPWM値で右タイヤを軸に後方へピボットターンする
 void Rotation::turnBackRightPivot(int angle, int pwm)
 {
-  double Distance = 2 * M_PI * TREAD * angle / 360;  //弧の長さ r:半径 angle:中心角
-  double targetDistance = Mileage::calculateWheelMileage(measurer.getLeftCount()) - Distance;
+  double distance = 2 * M_PI * TREAD * angle / 360;  //弧の長さ
+  double targetDistance = Mileage::calculateWheelMileage(measurer.getLeftCount()) - distance;
 
+  //目標距離を超えるまでループ
   while(true) {
-    double currentArcDistance = Mileage::calculateWheelMileage(measurer.getLeftCount());
+    double currentDistance = Mileage::calculateWheelMileage(measurer.getLeftCount());
 
-    if(currentArcDistance <= targetDistance) {
+    if(currentDistance <= targetDistance) {
       break;
     }
 
@@ -126,13 +137,14 @@ void Rotation::turnBackRightPivot(int angle, int pwm)
 //設定された角度とPWM値で左タイヤを軸に前方へピボットターンする
 void Rotation::turnForwardLeftPivot(int angle, int pwm)
 {
-  double Distance = 2 * M_PI * TREAD * angle / 360;  //弧の長さ r:半径 angle:中心角
-  double targetDistance = Mileage::calculateWheelMileage(measurer.getRightCount()) + Distance;
+  double distance = 2 * M_PI * TREAD * angle / 360;  //弧の長さ
+  double targetDistance = Mileage::calculateWheelMileage(measurer.getRightCount()) + distance;
 
+  //目標距離を超えるまでループ
   while(true) {
-    double currentArcDistance = Mileage::calculateWheelMileage(measurer.getRightCount());
+    double currentDistance = Mileage::calculateWheelMileage(measurer.getRightCount());
 
-    if(currentArcDistance >= targetDistance) {
+    if(currentDistance >= targetDistance) {
       break;
     }
 
@@ -150,13 +162,14 @@ void Rotation::turnForwardLeftPivot(int angle, int pwm)
 //設定された角度とPWM値で左タイヤを軸に後方へピボットターンする
 void Rotation::turnBackLeftPivot(int angle, int pwm)
 {
-  double Distance = 2 * M_PI * TREAD * angle / 360;  //弧の長さ r:半径 angle:中心角
-  double targetDistance = Mileage::calculateWheelMileage(measurer.getRightCount()) - Distance;
+  double distance = 2 * M_PI * TREAD * angle / 360;  //弧の長さ
+  double targetDistance = Mileage::calculateWheelMileage(measurer.getRightCount()) - distance;
 
+  //目標距離を超えるまでループ
   while(true) {
-    double currentArcDistance = Mileage::calculateWheelMileage(measurer.getRightCount());
+    double currentDistance = Mileage::calculateWheelMileage(measurer.getRightCount());
 
-    if(currentArcDistance <= targetDistance) {
+    if(currentDistance <= targetDistance) {
       break;
     }
 
