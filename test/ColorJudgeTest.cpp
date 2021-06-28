5925c6a3f4b17893ff3908cc841a0b3853d0c714
hiroto0927 <83230897+hiroto0927@users.noreply.github.com>
2021-06-29 03:44:56 +0900

    Update: 測定値によるテストケースの追加

--- test/ColorJudgeTest.cpp
+++ test/ColorJudgeTest.cpp
@@ -8,8 +8,7 @@
 #include <gtest/gtest.h>
 
 namespace etrobocon2021_test {
-  // 彩度の境界値テスト
-  TEST(getColorTest, getColorByBlack)
+  TEST(getColorTest, getColorTrueBlack)
   {
     rgb_raw_t rgb = { 0, 0, 0 };
     COLOR expected = COLOR::BLACK;
@@ -17,7 +16,167 @@ namespace etrobocon2021_test {
     EXPECT_EQ(expected, ColorJudge::getColor(rgb));
   }
 
-  TEST(getColorTest, getColorByMax)
+  TEST(getColorTest, getColorLightBlack)
+  {
+    rgb_raw_t rgb = { 39, 39, 58 };
+    COLOR expected = COLOR::BLACK;
+
+    EXPECT_EQ(expected, ColorJudge::getColor(rgb));
+  }
+
+  TEST(getColorTest, getColorDarkBlack)
+  {
+    rgb_raw_t rgb = { 4, 5, 8 };
+    COLOR expected = COLOR::BLACK;
+
+    EXPECT_EQ(expected, ColorJudge::getColor(rgb));
+  }
+
+  TEST(getColorTest, getColorLightRed)
+  {
+    rgb_raw_t rgb = { 119, 81, 109 };
+    COLOR expected = COLOR::RED;
+
+    EXPECT_EQ(expected, ColorJudge::getColor(rgb));
+  }
+
+  TEST(getColorTest, getColorDarkRed)
+  {
+    rgb_raw_t rgb = { 111, 19, 19 };
+    COLOR expected = COLOR::RED;
+
+    EXPECT_EQ(expected, ColorJudge::getColor(rgb));
+  }
+
+  TEST(getColorTest, getColorLightRed2)
+  {
+    rgb_raw_t rgb = { 103, 90, 128 };
+    COLOR expected = COLOR::RED;
+
+    EXPECT_EQ(expected, ColorJudge::getColor(rgb));
+  }
+
+  TEST(getColorTest, getColorDarkRed2)
+  {
+    rgb_raw_t rgb = { 93, 16, 16 };
+    COLOR expected = COLOR::RED;
+
+    EXPECT_EQ(expected, ColorJudge::getColor(rgb));
+  }
+
+  TEST(getColorTest, getColorLightYellow)
+  {
+    rgb_raw_t rgb = { 120, 108, 71 };
+    COLOR expected = COLOR::YELLOW;
+
+    EXPECT_EQ(expected, ColorJudge::getColor(rgb));
+  }
+
+  TEST(getColorTest, getColorDarkYellow)
+  {
+    rgb_raw_t rgb = { 120, 104, 8 };
+    COLOR expected = COLOR::YELLOW;
+
+    EXPECT_EQ(expected, ColorJudge::getColor(rgb));
+  }
+
+  TEST(getColorTest, getColorLightYellow2)
+  {
+    rgb_raw_t rgb = { 104, 96, 71 };
+    COLOR expected = COLOR::YELLOW;
+
+    EXPECT_EQ(expected, ColorJudge::getColor(rgb));
+  }
+
+  TEST(getColorTest, getColorDarkYellow2)
+  {
+    rgb_raw_t rgb = { 104, 92, 8 };
+    COLOR expected = COLOR::YELLOW;
+
+    EXPECT_EQ(expected, ColorJudge::getColor(rgb));
+  }
+
+  TEST(getColorTest, getColorLightGreen)
+  {
+    rgb_raw_t rgb = { 72, 100, 106 };
+    COLOR expected = COLOR::GREEN;
+
+    EXPECT_EQ(expected, ColorJudge::getColor(rgb));
+  }
+
+  TEST(getColorTest, getColorDarkGreen)
+  {
+    rgb_raw_t rgb = { 4, 75, 35 };
+    COLOR expected = COLOR::GREEN;
+
+    EXPECT_EQ(expected, ColorJudge::getColor(rgb));
+  }
+
+  TEST(getColorTest, getColorLightGreen2)
+  {
+    rgb_raw_t rgb = { 85, 94, 124 };
+    COLOR expected = COLOR::GREEN;
+
+    EXPECT_EQ(expected, ColorJudge::getColor(rgb));
+  }
+
+  TEST(getColorTest, getColorDarkGreen2)
+  {
+    rgb_raw_t rgb = { 4, 64, 29 };
+    COLOR expected = COLOR::GREEN;
+
+    EXPECT_EQ(expected, ColorJudge::getColor(rgb));
+  }
+
+  TEST(getColorTest, getColorLightBlue)
+  {
+    rgb_raw_t rgb = { 81, 92, 144 };
+    COLOR expected = COLOR::BLUE;
+
+    EXPECT_EQ(expected, ColorJudge::getColor(rgb));
+  }
+
+  TEST(getColorTest, getColorDarkBlue)
+  {
+    rgb_raw_t rgb = { 4, 45, 112 };
+    COLOR expected = COLOR::BLUE;
+
+    EXPECT_EQ(expected, ColorJudge::getColor(rgb));
+  }
+
+  TEST(getColorTest, getColorLightBlue2)
+  {
+    rgb_raw_t rgb = { 83, 89, 137 };
+    COLOR expected = COLOR::BLUE;
+
+    EXPECT_EQ(expected, ColorJudge::getColor(rgb));
+  }
+
+  TEST(getColorTest, getColorDarkBlue2)
+  {
+    rgb_raw_t rgb = { 4, 40, 103 };
+    COLOR expected = COLOR::BLUE;
+
+    EXPECT_EQ(expected, ColorJudge::getColor(rgb));
+  }
+
+  TEST(getColorTest, getColorLightWhite)
+  {
+    rgb_raw_t rgb = { 104, 101, 146 };
+    COLOR expected = COLOR::WHITE;
+
+    EXPECT_EQ(expected, ColorJudge::getColor(rgb));
+  }
+
+  TEST(getColorTest, getColorDarkWhite)
+  {
+    rgb_raw_t rgb = { 92, 94, 141 };
+    COLOR expected = COLOR::WHITE;
+
+    EXPECT_EQ(expected, ColorJudge::getColor(rgb));
+  }
+
+  TEST(getColorTest, getColorMaxRgb)
   {
     rgb_raw_t rgb = { 112, 107, 152 };
     COLOR expected = COLOR::WHITE;
