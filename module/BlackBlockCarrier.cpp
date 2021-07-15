561891eee9658963073a4b0f78633c20b122871d
yutotanaka24 <83375723+yutotanaka24@users.noreply.github.com>
2021-07-15 23:33:04 +0900

    定数名をアッパースネークに変更

--- module/BlackBlockCarrier.cpp
+++ module/BlackBlockCarrier.cpp
@@ -18,17 +18,17 @@ void BlackBlockCarrier::carryBlackBlock()
   StraightRunner straightrunner;
   LineTracer linetracer(false);
   //青の線を通過
-  straightrunner.runStraightToDistance(280, runPwm);
+  straightrunner.runStraightToDistance(280, RUN_PWM);
   //青の線まで
-  linetracer.run(750, 12, runPwm, gain);
+  linetracer.run(750, 12, RUN_PWM, gain);
   //青の線を通過
-  straightrunner.runStraightToDistance(250, runPwm);
+  straightrunner.runStraightToDistance(250, RUN_PWM);
   //黄色の円まで
-  linetracer.runToColor(targetBrightness, 60, rungain);
+  linetracer.runToColor(TARGET_BRIGHTNESS, 60, rungain);
   //黄色の円を通過
-  straightrunner.runStraightToDistance(circleDistance, 60);
+  straightrunner.runStraightToDistance(CIRCLE_DISTANCE, 60);
   //緑の円まで
-  linetracer.runToColor(targetBrightness, 60, rungain);
+  linetracer.runToColor(TARGET_BRIGHTNESS, 60, rungain);
   //緑の円の中心まで移動
   straightrunner.runStraightToDistance(60, 40);
   //右に９０度回頭
@@ -36,13 +36,13 @@ void BlackBlockCarrier::carryBlackBlock()
   //黒線まで直進
   straightrunner.runStraightToDistance(70, 20);
   //黒ブロック手前まで直進
-  linetracer.runToColor(targetBrightness, 30, rungain);
+  linetracer.runToColor(TARGET_BRIGHTNESS, 30, rungain);
   //目標の奥行まで直進
-  straightrunner.runStraightToDistance(firstBlackDistance, 45);
+  straightrunner.runStraightToDistance(FIRST_BLACK_DISTANCE, 45);
   //右に９０度ピボットターン
   rotation.turnForwardRightPivot(100, 40);
   //ビンゴの中心まで直進
-  straightrunner.runStraightToDistance(secondBlackDistance, 30);
+  straightrunner.runStraightToDistance(SECOND_BLACK_DISTANCE, 30);
   //黒のラインまで下がる
   straightrunner.runStraightToDistance(100, -30);
 }
