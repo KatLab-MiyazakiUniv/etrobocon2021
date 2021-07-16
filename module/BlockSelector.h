7a3f153354f93d0433d3dedbb75a8671301dd401
mutotaka0426 <hm17058@student.miyazaki-u.ac.jp>
2021-07-17 04:26:23 +0900

    シミュレータが起動しない問題の解決

--- module/BlockSelector.h
+++ module/BlockSelector.h
@@ -38,33 +38,9 @@ class BlockSelector {
   std::array<bool, static_cast<int>(CIRCLE_ID::ID7) + 1> arrivableCircles;
 
   // ブロックがなくなった際、到着可能になるサークル
-  const std::array<std::array<bool, static_cast<int>(CIRCLE_ID::ID7) + 1>,
-                   static_cast<int>(BLOCK_ID::ID7) + 1>
-      // ブロックが運搬された際に開放されるブロックサークル
-      OPEN_CIRCLE_ID = { {
-          { T, T, T, T, F, F, F, F },
-          { F, T, T, F, T, F, F, F },
-          { T, T, F, T, F, T, F, F },
-          { T, T, T, T, T, F, T, T },
-          { T, T, F, T, T, T, T, T },
-          { F, F, T, F, T, F, T, T },
-          { F, F, F, T, F, T, T, F },
-          { F, F, F, F, T, T, T, T },
-      } };
+  const std::array<std::array<bool, static_cast<int>(BLOCK_ID::ID7) + 1>, 8> OPEN_CIRCLE_ID;
   // ブロックが運搬された際に開放されるブロック
-  const std::array<std::array<bool, static_cast<int>(BLOCK_ID::ID7) + 1>,
-                   static_cast<int>(BLOCK_ID::ID7) + 1>
-      // ブロックが運搬された際に開放されるブロック
-      OPEN_BLOCK_ID = { {
-          { T, T, T, T, T, F, F, F },
-          { T, T, F, T, F, T, F, F },
-          { T, F, T, T, T, F, T, F },
-          { T, T, T, T, T, T, F, T },
-          { T, F, T, T, T, T, T, T },
-          { F, T, F, T, T, T, F, T },
-          { F, F, T, F, T, F, T, T },
-          { F, F, F, T, T, T, T, T },
-      } };
+  const std::array<std::array<bool, 8>, 8> OPEN_BLOCK_ID;
 
   /**
    * ブロックが運搬済みかを判定する
