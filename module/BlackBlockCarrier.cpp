7f7105a4e5c0ba1ce9b4b2d6042062e733af4909
kodama0720 <bnuy1221@gmail.com>
2021-08-05 14:32:26 +0900

    黒ブロック運搬の精度調整・曲線状な運搬

--- module/BlackBlockCarrier.cpp
+++ module/BlackBlockCarrier.cpp
@@ -9,38 +9,68 @@ void BlackBlockCarrier::carryBlackBlock()
 {
   constexpr int RUN_PWM = 50;
   constexpr int TARGET_BRIGHTNESS = 12;  //目標輝度
-  const PidGain CURVE_GAIN(4, 2, 2.2);   //カーブのライントレースに使用するゲイン
-  const PidGain RUN_GAIN(1.5, 1, 1.5);   //直進のライントレースに使用するゲイン
+  const PidGain CURVE_GAIN(3, 1, 1);     //カーブのライントレースに使用するゲイン
+  const PidGain RUN_GAIN(1, 0.8, 1);     //直進のライントレースに使用するゲイン
+  bool isLeftEdge = !IS_LEFT_COURSE;
   Rotation rotation;
-  StraightRunner straightrunner;
-  LineTracer linetracer(false);
+  StraightRunner straightRunner;
+  LineTracer lineTracer(isLeftEdge);
+  Measurer measurer;
+  Controller controller;
+  InCrossLeft inCrossLeft(lineTracer);
+  InCrossRight inCrossRight(lineTracer);
 
   //青の線を通過
-  straightrunner.runStraightToDistance(280, RUN_PWM);
+  lineTracer.run(250, 20, RUN_PWM, RUN_GAIN);
   //青の線まで
-  linetracer.run(750, TARGET_BRIGHTNESS, RUN_PWM, CURVE_GAIN);
+  lineTracer.run(650, TARGET_BRIGHTNESS, RUN_PWM, CURVE_GAIN);
   //青の線を通過
-  straightrunner.runStraightToDistance(250, RUN_PWM);
+  lineTracer.run(350, 20, RUN_PWM, RUN_GAIN);
   //黄色の円まで
-  linetracer.runToColor(TARGET_BRIGHTNESS, RUN_PWM + 10, RUN_GAIN);
+  lineTracer.runToColor(TARGET_BRIGHTNESS, RUN_PWM, RUN_GAIN);
   //黄色の円を通過
-  straightrunner.runStraightToDistance(150, RUN_PWM + 10);
+  straightRunner.runStraightToDistance(120, RUN_PWM + 10);
   //緑の円まで
-  linetracer.runToColor(TARGET_BRIGHTNESS, RUN_PWM + 10, RUN_GAIN);
+  lineTracer.runToColor(TARGET_BRIGHTNESS, RUN_PWM, RUN_GAIN);
   //緑の円の中心まで移動
-  straightrunner.runStraightToDistance(RUN_PWM, 40);
+  straightRunner.runStraightToDistance(RUN_PWM, 55);
   //右に９０度回頭
   rotation.rotateRight(90, 10);
+  // // *曲がる
+  // if(isLeftEdge) {
+  //   inCrossLeft.runLeft();
+  // } else {
+  //   inCrossRight.runRight();
+  // }
   //黒線まで直進
-  straightrunner.runStraightToDistance(70, 20);
-  //黒ブロック手前まで直進
-  linetracer.runToColor(TARGET_BRIGHTNESS, 30, RUN_GAIN);
-  //目標の奥行まで直進
-  straightrunner.runStraightToDistance(420, RUN_PWM - 5);
-  //右に９０度ピボットターン
-  rotation.turnForwardRightPivot(100, 40);
+  straightRunner.runStraightToDistance(70, 20);
+  //黒ブロック手前までライントレース
+  lineTracer.runToColor(TARGET_BRIGHTNESS, 30, RUN_GAIN);
+  // //目標の奥行まで直進
+  // straightRunner.runStraightToDistance(420, RUN_PWM - 5);
+  // controller.stopMotor();
+  // controller.sleep(2000);
+  // //右に９０度回頭
+  // rotation.rotateRight(90, 10);
+  //*弧を描いて曲がる
+  double startDiff = Mileage::calculateWheelMileage(measurer.getLeftCount())
+                     - Mileage::calculateWheelMileage(measurer.getRightCount());  //開始時の差
+  while(Mileage::calculateWheelMileage(measurer.getRightCount()) + startDiff
+        >= Mileage::calculateWheelMileage(measurer.getLeftCount()) - 220) {
+    //モータのPWM値をセット
+    controller.setRightMotorPwm(30);
+    controller.setLeftMotorPwm(43);
+    controller.sleep();
+  }
+  controller.stopMotor();
+
   //ビンゴの中心まで直進
-  straightrunner.runStraightToDistance(760, RUN_PWM - 20);
+  straightRunner.runStraightToDistance(400, RUN_PWM - 20);
   //黒のラインまで下がる
-  straightrunner.runStraightToDistance(100, -30);
+  straightRunner.runStraightToDistance(85, -20);
+
+  //ビンゴの中心まで直進
+  //straightRunner.runStraightToDistance(760, RUN_PWM - 20);
+  //黒のラインまで下がる
+  //straightRunner.runStraightToDistance(100, -30);
 }
