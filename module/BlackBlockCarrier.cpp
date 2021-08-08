f1eaa8f13186b250735ae46f28f7905180bbf5c1
Hisataka-Hagiyama <hisataka7@icloud.com>
2021-08-08 21:34:23 +0900

    Update::Rコースの処理を追加

--- module/BlackBlockCarrier.cpp
+++ module/BlackBlockCarrier.cpp
@@ -23,7 +23,7 @@ void BlackBlockCarrier::carryBlackBlock()
   InCrossRight inCrossRight(lineTracer);
 
   //青の線を通過
-  lineTracer.run(250, 20, RUN_PWM, RUN_STRAIGHT_PWM);
+  lineTracer.run(250, 20, RUN_STRAIGHT_PWM, RUN_GAIN);
   //青の線まで
   lineTracer.run(650, TARGET_BRIGHTNESS, RUN_CURVE_PWM, CURVE_GAIN);
   //青の線を通過
@@ -37,31 +37,25 @@ void BlackBlockCarrier::carryBlackBlock()
   //緑の円の中心まで移動
   straightRunner.runStraightToDistance(RUN_STRAIGHT_PWM, 55);
   //右に９０度回頭
-  rotation.rotateRight(90, 10);
-  // // *曲がる
-  // if(isLeftEdge) {
-  //   inCrossLeft.runLeft();
-  // } else {
-  //   inCrossRight.runRight();
-  // }
+  IS_LEFT_COURSE ? rotation.rotateRight(90, 10) : rotation.rotateLeft(90, 10);
   //黒線まで直進
   straightRunner.runStraightToDistance(70, 20);
   //黒ブロック手前までライントレース
   straightRunner.runStraightToColor(30);
-  // //目標の奥行まで直進
-  // straightRunner.runStraightToDistance(420, RUN_PWM - 5);
-  // controller.stopMotor();
-  // controller.sleep(2000);
-  // //右に９０度回頭
-  // rotation.rotateRight(90, 10);
   //*弧を描いて曲がる
-  double startDiff = Mileage::calculateWheelMileage(measurer.getLeftCount())
-                     - Mileage::calculateWheelMileage(measurer.getRightCount());  //開始時の差
-  while(Mileage::calculateWheelMileage(measurer.getRightCount()) + startDiff
-        >= Mileage::calculateWheelMileage(measurer.getLeftCount()) - 220) {
+  double startDiff
+      = IS_LEFT_COURSE
+            ? Mileage::calculateWheelMileage(measurer.getLeftCount())
+                  - Mileage::calculateWheelMileage(measurer.getRightCount())
+            : Mileage::calculateWheelMileage(measurer.getRightCount())
+                  - Mileage::calculateWheelMileage(measurer.getLeftCount());  //開始時の差
+  while(IS_LEFT_COURSE ? Mileage::calculateWheelMileage(measurer.getRightCount()) + startDiff
+                             >= Mileage::calculateWheelMileage(measurer.getLeftCount()) - 220
+                       : Mileage::calculateWheelMileage(measurer.getLeftCount()) + startDiff
+                             >= Mileage::calculateWheelMileage(measurer.getRightCount()) - 220) {
     //モータのPWM値をセット
-    controller.setRightMotorPwm(41);
-    controller.setLeftMotorPwm(58);
+    controller.setRightMotorPwm(IS_LEFT_COURSE ? 41 : 58);
+    controller.setLeftMotorPwm(IS_LEFT_COURSE ? 58 : 41);
     controller.sleep();
   }
   controller.stopMotor();
@@ -70,9 +64,4 @@ void BlackBlockCarrier::carryBlackBlock()
   straightRunner.runStraightToDistance(400, RUN_STRAIGHT_PWM - 20);
   //黒のラインまで下がる
   straightRunner.runStraightToDistance(85, -20);
-
-  //ビンゴの中心まで直進
-  // straightRunner.runStraightToDistance(760, RUN_PWM - 20);
-  //黒のラインまで下がる
-  // straightRunner.runStraightToDistance(100, -30);
 }
