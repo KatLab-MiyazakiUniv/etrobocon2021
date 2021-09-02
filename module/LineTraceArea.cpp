016f7e9e6cbfa67b000b8b1d26de5a33d2594d80
uchiyam <yamayamato221@outlook.jp>
2021-09-02 18:06:31 +0900

    パラメータ調整

--- module/LineTraceArea.cpp
+++ module/LineTraceArea.cpp
@@ -10,11 +10,11 @@
 const std::array<SectionParam, LineTraceArea::LEFT_SECTION_SIZE> LineTraceArea::LEFT_COURSE_INFO
     = { SectionParam{ 150, 12, 60, PidGain(0.1, 0.8, 0.1) },
         SectionParam{ 1445, 12, 100, PidGain(3.5, 1, 1) },
-        SectionParam{ 670, 12, 80, PidGain(1.3, 0.8, 1.6) },  // 1.2
+        SectionParam{ 670, 12, 80, PidGain(1.3, 0.8, 1.6) },
         SectionParam{ 600, 12, 100, PidGain(3.3, 2, 1.2) },
-        SectionParam{ 580, 12, 80, PidGain(0.95, 0.66, 0.66) },  // 0.95, 0.66, 0.66
-        SectionParam{ 1450, 12, 100, PidGain(3.5, 0.3, 0.3) },
-        SectionParam{ 400, 12, 60, PidGain(4, 0.8, 0.1) },
+        SectionParam{ 580, 12, 80, PidGain(0.95, 0.675, 0.67) },
+        SectionParam{ 1450, 12, 100, PidGain(3.75, 0.3, 0.3) },
+        SectionParam{ 400, 12, 60, PidGain(4, 1.2, 0.1) }, //(4, 0.8, 0.1)
         SectionParam{ 700, 12, 100, PidGain(3, 1, 1.8) } };
 
 // Rコースの情報を初期化する
