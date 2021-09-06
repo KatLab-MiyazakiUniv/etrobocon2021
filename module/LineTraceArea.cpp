269f2f88b7cece3d4dd42d3d006dffedcf55c9c2
uchiyam <yamayamato221@outlook.jp>
2021-09-06 19:11:53 +0900

    パラメータ調整

--- module/LineTraceArea.cpp
+++ module/LineTraceArea.cpp
@@ -10,10 +10,10 @@
 const std::array<SectionParam, LineTraceArea::LEFT_SECTION_SIZE> LineTraceArea::LEFT_COURSE_INFO
     = { SectionParam{ 150, 12, 60, PidGain(0.1, 0.8, 0.1) },
         SectionParam{ 1445, 12, 100, PidGain(3.5, 1, 1) },
-        SectionParam{ 670, 12, 80, PidGain(1.3, 0.83, 1.3) },
+        SectionParam{ 670, 12, 80, PidGain(1.5, 0.83, 1.3) },
         SectionParam{ 600, 12, 100, PidGain(3.3, 1, 1) },
-        SectionParam{ 580, 12, 80, PidGain(0.95, 0.67, 0.66) },
-        SectionParam{ 1450, 12, 100, PidGain(4, 0.3, 0.3) },
+        SectionParam{ 580, 12, 80, PidGain(1.5, 0.67, 0.66) },
+        SectionParam{ 1450, 12, 100, PidGain(3, 0.3, 0.3) },
         SectionParam{ 400, 12, 60, PidGain(4.1, 1.2, 0.1) },
         SectionParam{ 700, 12, 100, PidGain(3, 1, 1.8) } };
 
