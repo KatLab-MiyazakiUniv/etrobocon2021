59ae161e0571c74ff07b420cb866c704b08f27e6
kodama0720 <bnuy1221@gmail.com>
2021-10-25 11:07:40 +0900

    CIでの精度確認のため変更前に戻した

--- module/BingoMotion/BlackBlockCarrier.cpp
+++ module/BingoMotion/BlackBlockCarrier.cpp
@@ -10,7 +10,6 @@ void BlackBlockCarrier::carryBlackBlock()
   constexpr int RUN_STRAIGHT_PWM = 80;
   constexpr int RUN_CURVE_PWM = 50;
   constexpr int TARGET_BRIGHTNESS = 20;  //目標輝度
-  constexpr int BLUE_WHITE_BRIGHTNESS = 25;
   bool isLeftEdge = !IS_LEFT_COURSE;
   Rotation rotation;
   StraightRunner straightRunner;
@@ -18,18 +17,18 @@ void BlackBlockCarrier::carryBlackBlock()
   Measurer measurer;
   Controller controller;
   //青の線までライントレース
-  lineTracer.runToColor(TARGET_BRIGHTNESS, RUN_STRAIGHT_PWM, PidGain(0.1, 0.8, 0.12));
+  lineTracer.runToColor(TARGET_BRIGHTNESS, RUN_STRAIGHT_PWM - 40, PidGain(0.1, 0.8, 0.12));
   //青の線を通過
-  lineTracer.run(150, BLUE_WHITE_BRIGHTNESS, RUN_STRAIGHT_PWM, PidGain(0.12, 0.8, 0.12));
+  lineTracer.run(140, TARGET_BRIGHTNESS, RUN_STRAIGHT_PWM - 30, PidGain(0.12, 0.8, 0.12));
   //青の線まで
-  lineTracer.run(770, TARGET_BRIGHTNESS, RUN_CURVE_PWM, PidGain(0.4, 0.8, 0.2));
+  lineTracer.run(780, TARGET_BRIGHTNESS, RUN_CURVE_PWM, PidGain(0.4, 0.8, 0.2));
   //青の線を通過
-  lineTracer.run(150, BLUE_WHITE_BRIGHTNESS, RUN_STRAIGHT_PWM, PidGain(0.19, 0.8, 0.19));
+  lineTracer.run(200, TARGET_BRIGHTNESS, RUN_STRAIGHT_PWM - 20, PidGain(0.19, 0.8, 0.19));
   //黄色の円まで
-  lineTracer.run(200, TARGET_BRIGHTNESS, RUN_STRAIGHT_PWM, PidGain(0.12, 0.8, 0.15));
-  lineTracer.runToColor(TARGET_BRIGHTNESS, RUN_STRAIGHT_PWM - 20, PidGain(0.15, 0.8, 0.2));
+  lineTracer.run(200, TARGET_BRIGHTNESS, RUN_STRAIGHT_PWM - 20, PidGain(0.12, 0.8, 0.15));
+  lineTracer.runToColor(TARGET_BRIGHTNESS, RUN_STRAIGHT_PWM - 40, PidGain(0.15, 0.8, 0.2));
   //黄色の円を通過
-  straightRunner.runStraightToDistance(120, RUN_STRAIGHT_PWM - 20);
+  straightRunner.runStraightToDistance(120, RUN_STRAIGHT_PWM - 30);
   //緑の円まで
   lineTracer.run(300, TARGET_BRIGHTNESS, RUN_STRAIGHT_PWM - 30, PidGain(0.12, 0.8, 0.2));
   lineTracer.run(100, TARGET_BRIGHTNESS, RUN_STRAIGHT_PWM - 45, PidGain(0.12, 0.8, 0.15));
@@ -37,41 +36,31 @@ void BlackBlockCarrier::carryBlackBlock()
   //９０度ピボットターン
   straightRunner.runStraightToDistance(15, RUN_STRAIGHT_PWM - 60);
   controller.sleep(500);
-  IS_LEFT_COURSE ? rotation.turnForwardRightPivot(91, 33) : rotation.turnForwardLeftPivot(91, 33);
+  IS_LEFT_COURSE ? rotation.turnForwardRightPivot(91.3, 33)
+                 : rotation.turnForwardLeftPivot(91.3, 33);
   //黒ブロック手前までライントレース
   lineTracer.run(85, TARGET_BRIGHTNESS, 20, PidGain(0.11, 0.1, 0.11));
-  lineTracer.runToColor(TARGET_BRIGHTNESS, RUN_STRAIGHT_PWM - 60, PidGain(0.1, 1, 0.11));
+  //センターマークの平行線上まで直進
   straightRunner.runStraightToDistance(100, RUN_STRAIGHT_PWM - 50);
-  straightRunner.runStraightToDistance(120, RUN_STRAIGHT_PWM - 40);
-
-  //弧を描いて曲がる
-  double startDiff
-      = IS_LEFT_COURSE
-            ? Mileage::calculateWheelMileage(measurer.getLeftCount())
-                  - Mileage::calculateWheelMileage(measurer.getRightCount())
-            : Mileage::calculateWheelMileage(measurer.getRightCount())
-                  - Mileage::calculateWheelMileage(measurer.getLeftCount());  //開始時の差
-  while(IS_LEFT_COURSE ? Mileage::calculateWheelMileage(measurer.getRightCount()) + startDiff
-                             >= Mileage::calculateWheelMileage(measurer.getLeftCount()) - 150
-                       : Mileage::calculateWheelMileage(measurer.getLeftCount()) + startDiff
-                             >= Mileage::calculateWheelMileage(measurer.getRightCount()) - 150) {
-    //モータのPWM値をセット
-    controller.setRightMotorPwm(IS_LEFT_COURSE ? 50 : 80);
-    controller.setLeftMotorPwm(IS_LEFT_COURSE ? 80 : 50);
-    controller.sleep();
-  }
-  while(IS_LEFT_COURSE ? Mileage::calculateWheelMileage(measurer.getRightCount()) + startDiff
-                             >= Mileage::calculateWheelMileage(measurer.getLeftCount()) - 203
-                       : Mileage::calculateWheelMileage(measurer.getLeftCount()) + startDiff
-                             >= Mileage::calculateWheelMileage(measurer.getRightCount()) - 203) {
-    //モータのPWM値をセット
-    controller.setRightMotorPwm(IS_LEFT_COURSE ? 30 : 50);
-    controller.setLeftMotorPwm(IS_LEFT_COURSE ? 50 : 30);
-    controller.sleep();
-  }
-  straightRunner.runStraightToDistance(150, RUN_STRAIGHT_PWM - 30);
+  straightRunner.runStraightToDistance(100, RUN_STRAIGHT_PWM - 40);
+  straightRunner.runStraightToDistance(240, RUN_STRAIGHT_PWM - 30);
+  straightRunner.runStraightToDistance(75, RUN_STRAIGHT_PWM - 50);
+  controller.sleep(500);
+  // 90度ピボットターン
+  IS_LEFT_COURSE ? rotation.turnForwardRightPivot(90, 20) : rotation.turnForwardLeftPivot(90, 20);
+  controller.sleep(300);
+  //センターマークまで直進する(黒→青→青→黒の順に認識する)
+  straightRunner.runStraightToDistance(25, RUN_STRAIGHT_PWM - 70);
+  straightRunner.runStraightToDistance(25, RUN_STRAIGHT_PWM - 60);
+  straightRunner.runStraightToDistance(20, RUN_STRAIGHT_PWM - 50);
+  straightRunner.runStraightToColor(RUN_STRAIGHT_PWM - 40, COLOR::BLACK);
+  straightRunner.runStraightToColor(RUN_STRAIGHT_PWM - 30, COLOR::BLUE);
+  straightRunner.runStraightToColor(RUN_STRAIGHT_PWM - 30, COLOR::BLUE);
   straightRunner.runStraightToColor(RUN_STRAIGHT_PWM - 30, COLOR::BLACK);
-  straightRunner.runStraightToDistance(125, RUN_STRAIGHT_PWM - 50);
-  //黒のラインまで下がる
-  straightRunner.runStraightToDistance(40, -20);
+  straightRunner.runStraightToDistance(135, RUN_STRAIGHT_PWM - 50);
+  //タイヤの中心を黒線に合わせる
+  controller.sleep(500);
+  straightRunner.runStraightToColor(-20, COLOR::BLACK);
+  controller.sleep(200);
+  straightRunner.runStraightToDistance(35, RUN_STRAIGHT_PWM - 60);
 }
