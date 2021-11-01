ad88f53eb2e733c42ad5ce9f92c89d2008674732
Takahiro55555 <tkhr.corei7@gmail.com>
2021-11-02 00:59:01 +0900

    update: 実装の違いによる実行時間の差を確認

--- course_info_generator/course_info_generator.py
+++ course_info_generator/course_info_generator.py
@@ -41,5 +41,43 @@ def init_database(cursor, sql_file):
         sql = f.read()
         cursor.executescript(sql)
 
+def countup_all_info_01(fname):
+    tables = ('left_info', 'right_info')
+    columns = ('RED1', 'RED2', 'YELLOW1', 'YELLOW2', 'BLUE1', 'BLUE2', 'GREEN1', 'GREEN2')
+    sql_template = "UPDATE %s SET %s_ACCESS_COUNTER = %s_ACCESS_COUNTER + 1 WHERE ID = %s"
+    for table in tables:
+        for id in range(1, 40320 + 1):
+            for col in columns:
+                # この実装の場合、実行に2時間弱の時間を要した
+                # 家のPCでは、CPUがボトルネックになっていた(Core i5 4670)
+                con = sqlite3.connect(fname)
+                cursor = con.cursor()
+                sql = sql_template % (table, col, col, id)
+                cursor.execute(sql)
+                cursor.close()
+                con.commit()
+                con.close()
+
+def countup_all_info_02(fname):
+    tables = ('left_info', 'right_info')
+    columns = ('RED1', 'RED2', 'YELLOW1', 'YELLOW2', 'BLUE1', 'BLUE2', 'GREEN1', 'GREEN2')
+    sql_template = "UPDATE %s SET %s_ACCESS_COUNTER = %s_ACCESS_COUNTER + 1 WHERE ID = %s"
+    
+    # この実装の場合、実行に20秒弱の時間を要した
+    # 実行環境は、countup_all_info_01 の場合と同じ
+    con = sqlite3.connect(fname)
+    cursor = con.cursor()
+    for table in tables:
+        for id in range(1, 40320 + 1):
+            for col in columns:
+                sql = sql_template % (table, col, col, id)
+                cursor.execute(sql)
+    cursor.close()
+    con.commit()
+    con.close()
+
 if __name__ == '__main__':
-    main('all_pattern_course_info.sqlite3')
+    fname = 'all_pattern_course_info.sqlite3'
+    main(fname)
+    # countup_all_info_01(fname)
+    countup_all_info_02(fname)
