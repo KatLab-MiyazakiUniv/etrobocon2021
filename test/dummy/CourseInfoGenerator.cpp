422a1e869c5cff6946542b0259a3b5f8e4131be2
Takahiro55555 <tkhr.corei7@gmail.com>
2021-11-02 03:24:52 +0900

    update: 最低限の実装を行い、ビルドの成功まで確認

--- test/dummy/CourseInfoGenerator.cpp
+++ test/dummy/CourseInfoGenerator.cpp
@@ -27,7 +27,87 @@ CourseInfoGenerator* CourseInfoGenerator::getInstance()
   }
 }
 
+int CourseInfoGenerator::getCourseInfoL(ETROBOC_COURSE_INFO_ID id)
+{
+  int circleIdIndex = 0;
+  if(readBlockInfoL == READ_ALL_MASK) {
+    readBlockInfoL = NOT_READ_MASK;
+    currentBlockPatternindexL = (currentBlockPatternindexL + 1) % allBlockPatterns.size();
+  }
+  if(id == ETROBOC_COURSE_INFO_BLOCK_POS_BLACK1) {
+    return 48;
+  } else if(id == ETROBOC_COURSE_INFO_BLOCK_POS_RED1) {
+    circleIdIndex = allBlockPatterns[currentBlockPatternindexL][0];
+    readBlockInfoL |= READ_RED1_MASK;
+  } else if(id == ETROBOC_COURSE_INFO_BLOCK_POS_RED2) {
+    circleIdIndex = allBlockPatterns[currentBlockPatternindexL][1];
+    readBlockInfoL |= READ_RED2_MASK;
+  } else if(id == ETROBOC_COURSE_INFO_BLOCK_POS_YELLOW1) {
+    circleIdIndex = allBlockPatterns[currentBlockPatternindexL][2];
+    readBlockInfoL |= READ_YELLOW1_MASK;
+  } else if(id == ETROBOC_COURSE_INFO_BLOCK_POS_YELLOW2) {
+    circleIdIndex = allBlockPatterns[currentBlockPatternindexL][3];
+    readBlockInfoL |= READ_YELLOW2_MASK;
+  } else if(id == ETROBOC_COURSE_INFO_BLOCK_POS_BLUE1) {
+    circleIdIndex = allBlockPatterns[currentBlockPatternindexL][4];
+    readBlockInfoL |= READ_BLUE1_MASK;
+  } else if(id == ETROBOC_COURSE_INFO_BLOCK_POS_BLUE2) {
+    circleIdIndex = allBlockPatterns[currentBlockPatternindexL][5];
+    readBlockInfoL |= READ_BLUE2_MASK;
+  } else if(id == ETROBOC_COURSE_INFO_BLOCK_POS_GREEN1) {
+    circleIdIndex = allBlockPatterns[currentBlockPatternindexL][6];
+    readBlockInfoL |= READ_GREEN1_MASK;
+  } else if(id == ETROBOC_COURSE_INFO_BLOCK_POS_GREEN2) {
+    circleIdIndex = allBlockPatterns[currentBlockPatternindexL][7];
+    readBlockInfoL |= READ_GREEN2_MASK;
+  } else {
+    return -1;
+  }
+  return CROSS_CIRCLE_ID_LISTS_L[circleIdIndex];
+}
+
+int CourseInfoGenerator::getCourseInfoR(ETROBOC_COURSE_INFO_ID id)
+{
+  int circleIdIndex = 0;
+  if(readBlockInfoR == READ_ALL_MASK) {
+    readBlockInfoR = NOT_READ_MASK;
+    currentBlockPatternindexR = (currentBlockPatternindexR + 1) % allBlockPatterns.size();
+  }
+  if(id == ETROBOC_COURSE_INFO_BLOCK_POS_BLACK1) {
+    return 48;
+  } else if(id == ETROBOC_COURSE_INFO_BLOCK_POS_RED1) {
+    circleIdIndex = allBlockPatterns[currentBlockPatternindexR][0];
+    readBlockInfoR |= READ_RED1_MASK;
+  } else if(id == ETROBOC_COURSE_INFO_BLOCK_POS_RED2) {
+    circleIdIndex = allBlockPatterns[currentBlockPatternindexR][1];
+    readBlockInfoR |= READ_RED2_MASK;
+  } else if(id == ETROBOC_COURSE_INFO_BLOCK_POS_YELLOW1) {
+    circleIdIndex = allBlockPatterns[currentBlockPatternindexR][2];
+    readBlockInfoR |= READ_YELLOW1_MASK;
+  } else if(id == ETROBOC_COURSE_INFO_BLOCK_POS_YELLOW2) {
+    circleIdIndex = allBlockPatterns[currentBlockPatternindexR][3];
+    readBlockInfoR |= READ_YELLOW2_MASK;
+  } else if(id == ETROBOC_COURSE_INFO_BLOCK_POS_BLUE1) {
+    circleIdIndex = allBlockPatterns[currentBlockPatternindexR][4];
+    readBlockInfoR |= READ_BLUE1_MASK;
+  } else if(id == ETROBOC_COURSE_INFO_BLOCK_POS_BLUE2) {
+    circleIdIndex = allBlockPatterns[currentBlockPatternindexR][5];
+    readBlockInfoR |= READ_BLUE2_MASK;
+  } else if(id == ETROBOC_COURSE_INFO_BLOCK_POS_GREEN1) {
+    circleIdIndex = allBlockPatterns[currentBlockPatternindexR][6];
+    readBlockInfoR |= READ_GREEN1_MASK;
+  } else if(id == ETROBOC_COURSE_INFO_BLOCK_POS_GREEN2) {
+    circleIdIndex = allBlockPatterns[currentBlockPatternindexR][7];
+    readBlockInfoR |= READ_GREEN2_MASK;
+  } else {
+    return -1;
+  }
+  return CROSS_CIRCLE_ID_LISTS_R[circleIdIndex];
+}
+
 CourseInfoGenerator::CourseInfoGenerator()
+  : CROSS_CIRCLE_ID_LISTS_L((69, 77, 66, 75, 68, 82, 71, 80)),
+    CROSS_CIRCLE_ID_LISTS_R((72, 74, 67, 76, 65, 81, 70, 83))
 {
   /* 何もしない */
 }
@@ -48,4 +128,10 @@ CourseInfoGenerator& CourseInfoGenerator::operator=(const CourseInfoGenerator& s
 void CourseInfoGenerator::_initialize()
 {
   /* ToDo: 初期化処理を実装する */
+  std::vector<int> baseCase = { 0, 1, 2, 3, 4, 5, 6, 7 };
+  do {
+    for(auto num : baseCase) {
+      allBlockPatterns.push_back(baseCase);
+    }
+  } while(std::next_permutation(baseCase.begin(), baseCase.end()));
 }
--- test/dummy/CourseInfoGenerator.h
+++ test/dummy/CourseInfoGenerator.h
@@ -2,11 +2,15 @@
  * @file CourseInfoGenerator.h
  * @brief GoogleTestにおいて、全てのブロック配置パターンを網羅するためのシングルトンクラス
  * @author Takahiro55555
- * @ref 
+ * @ref
  */
 
 #ifndef COURSE_INFO_GENERATOR_H
 #define COURSE_INFO_GENERATOR_H
+
+#include <vector>
+#include <algorithm>
+#include "etroboc_ext.h"
 
 class CourseInfoGenerator {
  public:
@@ -22,12 +26,74 @@ class CourseInfoGenerator {
    */
   static CourseInfoGenerator* getInstance();
 
+  /**
+   * コース情報の取得(L)
+   * @param id
+   * @return 位置情報(ASCIIコード)
+   */
+  int getCourseInfoL(ETROBOC_COURSE_INFO_ID id);
+
+  /**
+   * コース情報の取得(R)
+   * @param id
+   * @return 位置情報(ASCIIコード)
+   */
+  int getCourseInfoR(ETROBOC_COURSE_INFO_ID id);
 
  private:
   /**
    * シングルトン用インスタンス
    */
   static CourseInfoGenerator* _singleton;
+
+  /**
+   * ビット演算用マスク
+   */
+  const uint8_t READ_RED1_MASK = 0b00000001;
+  const uint8_t READ_RED2_MASK = 0b00000010;
+  const uint8_t READ_YELLOW1_MASK = 0b00000100;
+  const uint8_t READ_YELLOW2_MASK = 0b00001000;
+  const uint8_t READ_BLUE1_MASK = 0b00010000;
+  const uint8_t READ_BLUE2_MASK = 0b00100000;
+  const uint8_t READ_GREEN1_MASK = 0b01000000;
+  const uint8_t READ_GREEN2_MASK = 0b10000000;
+  const uint8_t READ_ALL_MASK = 0b11111111;
+  const uint8_t NOT_READ_MASK = 0b00000000;
+
+  /**
+   * シミュレータ用APIにおける交点サークルの座標ID(L)
+   */
+  const std::vector<int> CROSS_CIRCLE_ID_LISTS_L;
+
+  /**
+   * シミュレータ用APIにおける交点サークルの座標ID(R)
+   */
+  const std::vector<int> CROSS_CIRCLE_ID_LISTS_R;
+
+  /**
+   * 全パターンのブロック配置を持つ
+   */
+  std::vector<std::vector<int>> allBlockPatterns;
+
+  /**
+   * allBlockPatterns における現在返すべき情報の添え字(L)
+   */
+  int currentBlockPatternindexL = 0;
+
+  /**
+   * allBlockPatterns における現在返すべき情報の添え字(R)
+   */
+  int currentBlockPatternindexR = 0;
+
+  /**
+   * 配置情報取得済みブロックの情報を保持する(L)
+   */
+  uint8_t readBlockInfoL = NOT_READ_MASK;
+
+  /**
+   * 配置情報取得済みブロックの情報を保持する(R)
+   */
+  uint8_t readBlockInfoR = NOT_READ_MASK;
 
   /**
    * コピーコンストラクタ
@@ -45,10 +111,11 @@ class CourseInfoGenerator {
    * 初期化する
    */
   void _initialize();
+
+  CourseInfoGenerator();
+
+//   ~CourseInfoGenerator();
 };
 
-CourseInfoGenerator::CourseInfoGenerator(/* args */) {}
-
-CourseInfoGenerator::~CourseInfoGenerator() {}
 
 #endif
